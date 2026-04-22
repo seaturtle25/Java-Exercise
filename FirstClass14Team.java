b34c33b222a27a18724b9da4c8b12c2c8c22f017
Lily Ou <lilyou0721@gmail.com>
2026-04-22 14:37:43 +0800

    Update FirstClass14Team.java

--- FirstClass14Team.java
+++ FirstClass14Team.java
@@ -2,5 +2,6 @@ public class FirstClass14Team {
     public static void main(String[] args) {
         System.out.println("這是用練習GitHub協作的程式!");
         System.out.println("\"學號:\"01357014\"\n姓名:\"丁宥寧\"");
+        System.out.println("\"學號:\"01357032\"\n姓名:\"歐幸慈\"");
     }
 }
