0e861893854dd260476220b4d66d5332d2215c00
丁宥寧 <rubyding0525@gmail.com>
2026-04-22 14:24:13 +0800

    feat: 完成程式骨幹並增加丁宥寧資料

---
+++ FirstClass14Team.java
@@ -0,0 +1,6 @@
+public class FirstClass14Team {
+    public static void main(String[] args) {
+        System.out.println("這是用練習GitHub協作的程式!");
+        System.out.println("\"學號:\"01357014\"\n姓名:\"丁宥寧\"");
+    }
+}
