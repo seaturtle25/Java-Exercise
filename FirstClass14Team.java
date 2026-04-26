48cd2c294113519acb51907018c18487605eb5da
01357136 <01357136@email.ntou.edu.tw>
2026-04-26 21:32:33 +0800

    third version
    print the third member name

--- FirstClass14Team.java
+++ FirstClass14Team.java
@@ -3,5 +3,6 @@ public class FirstClass14Team {
         System.out.println("這是用練習GitHub協作的程式!");
         System.out.println("\"學號:\"01357014\"\n姓名:\"丁宥寧\"");
         System.out.println("\"學號:\"01357032\"\n姓名:\"歐幸慈\"");
+        System.out.println("\"學號:\"01357136\"\n姓名:\"楊孟芸\"");
     }
 }
